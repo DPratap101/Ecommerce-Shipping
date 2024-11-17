8c43d412c679bed347088afad10725a25359bc2b
Dpratap <dpratap.360@gmail.com>
2024-11-17 22:24:59 +0530

    1.2.2 shipping functionality added

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const warehouseController = require('../controllers/warehouse.controller');
-
-
+const shippingController = require("../controllers/shipping.controller")
+const calculateShipping = require("../controllers/calculateShipping.controller")
 const router = express.Router();
 
 // Warehouse Routes
@@ -9,6 +9,7 @@ router.get('/warehouses/nearest', warehouseController.getNearestWarehouse);
 router.get('/warehouses', warehouseController.getAllWarehouses);
 router.post('/warehouses', warehouseController.createWarehouse);
 
-
+router.get('/shipping-charge', shippingController.getShippingCharge);
+router.get('/shipping-charge/calculate', calculateShipping.calculateShippingCharge);
 
 module.exports = router;
